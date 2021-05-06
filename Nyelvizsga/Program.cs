c8bb5b384bf692e59190b2a73fb9529814bf0d74
Axmart <notaxmart@gmail.com>
2021-05-07 01:17:06 +0200

    Add project files.

---
+++ Nyelvizsga/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nyelvizsga
+{
+    class Program
+    {
+        public static List<Vizsga> Vizsgak;
+        public static int bekertEv;
+
+        static void Main(string[] args)
+        {
+            Vizsgak = Vizsga.beolvasas();
+            legnepszerubbVizsgak();
+            bekertEv = evBekeres();
+            legnagyobbSikertelenVizsgak();
+            vizsgazoNelkuliNyelv();
+            osszesites();
+
+            Console.ReadKey();
+        }
+
+        private static void osszesites()
+        {
+            var sw = new StreamWriter(@"../../res/osszesites.csv");
+
+            foreach (var nyelv in Vizsgak)
+            {
+                sw.WriteLine($"{nyelv.Nyelv};{nyelv.SikeresekSzama.Values.Sum() + nyelv.SikertelenekSzama.Values.Sum()};{((double)nyelv.SikeresekSzama.Values.Sum() / (nyelv.SikeresekSzama.Values.Sum() + nyelv.SikertelenekSzama.Values.Sum())) * 100:0.00}%");
+
+            }
+            sw.Close();
+
+        }
+
+        private static void vizsgazoNelkuliNyelv()
+        {
+            Console.WriteLine("5. feladat:");
+            int vizsgazok = 0;
+            foreach (var nyelv in Vizsgak)
+            {
+                if (nyelv.SikeresekSzama[bekertEv] == 0 && nyelv.SikertelenekSzama[bekertEv] == 0)
+                {
+                    Console.WriteLine("\t"+nyelv.Nyelv);
+                    vizsgazok++;
+                }
+            }
+
+            if (vizsgazok == 0)
+            {
+                Console.WriteLine("Minden nyelvbol volt vizsgazo");
+            }
+        }
+
+        private static void legnagyobbSikertelenVizsgak()
+        {
+            Console.WriteLine("4. feladat: ");
+
+
+            string tempNev = "";
+            double tempArany = 0;
+            foreach (var vizsga in Vizsgak)
+            {
+                //Console.WriteLine((double)vizsga.SikertelenekSzama[bekertEv] +"\t"+ ((double)vizsga.SikeresekSzama[bekertEv] + (double)vizsga.SikertelenekSzama[bekertEv]));
+                if (tempArany < (double)vizsga.SikertelenekSzama[bekertEv] / ((double)vizsga.SikeresekSzama[bekertEv]+ (double)vizsga.SikertelenekSzama[bekertEv]))
+                {
+                    tempArany = (double)vizsga.SikertelenekSzama[bekertEv] / ((double)vizsga.SikeresekSzama[bekertEv] + (double)vizsga.SikertelenekSzama[bekertEv]);
+                    tempNev = vizsga.Nyelv;
+                }
+
+            }
+            Console.WriteLine($"{bekertEv}-ben a {tempNev} nyelvbol a sikertelen vizsgak aranya {tempArany*100:0.00}%");
+
+
+        }
+
+        private static int evBekeres()
+        {
+            Console.Write("3. feladat: \n\tVizsgalando ev: ");
+            int ev = Int32.Parse(Console.ReadLine());
+            while ( ev < 2009 || ev > 2018  )
+            {
+                ev = Int32.Parse(Console.ReadLine());
+            }
+
+            return ev;
+        }
+
+        private static void legnepszerubbVizsgak()
+        {
+
+            Console.WriteLine("2. feladat: A legnepszerubb nyelvek: ");
+            var nepszeruNyelvek =  Vizsgak.OrderByDescending(x => x.SikeresekSzama.Values.Sum() + x.SikertelenekSzama.Values.Sum()).Select(x => new { nev = x.Nyelv, vizsgakSzama = x.SikeresekSzama.Values.Sum() + x.SikertelenekSzama.Values.Sum() }).Take(3).ToList();
+            foreach (var nyelv in nepszeruNyelvek)
+            {
+                Console.WriteLine($"\t{nyelv.nev}\t{nyelv.vizsgakSzama}");
+            }
+
+
+            //Nem LINQ
+
+            //var vizsgakSzama = new Dictionary<string, int>();
+            //int tempOsszeg = 0;
+            //string tempNev;
+            //foreach (var vizsga in Vizsgak)
+            //{
+            //    tempNev = vizsga.Nyelv;
+            //    foreach (var evszam in vizsga.SikeresekSzama)
+            //    {
+            //        tempOsszeg += evszam.Value;
+            //    }
+            //    foreach (var evszam in vizsga.SikertelenekSzama)
+            //    {
+            //        tempOsszeg += evszam.Value;
+            //    }
+            //    vizsgakSzama.Add(tempNev, tempOsszeg);
+            //    tempOsszeg = 0;
+
+            //}
+
+            //List<string> legnepszerubb = new List<string>();
+            //string nyelvTemp = "";
+            //for (int i = 0; i < 3; i++)
+            //{
+
+            //    foreach (var v in vizsgakSzama)
+            //    {
+            //        if (tempOsszeg < v.Value)
+            //        {
+            //            nyelvTemp = v.Key;
+            //            tempOsszeg = v.Value;
+            //        }
+
+            //    }
+            //    legnepszerubb.Add(nyelvTemp);
+            //    vizsgakSzama.Remove(nyelvTemp);
+            //    tempOsszeg = 0;
+            //}
+
+            //foreach (var top in legnepszerubb)
+            //{
+            //    Console.WriteLine("\t"+top);
+            //}
+
+
+
+
+        }
+    }
+}
---
+++ Nyelvizsga/Vizsga.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nyelvizsga
+{
+    class Vizsga
+    {
+
+        public string Nyelv { get; set; }
+        public Dictionary<int, int> SikeresekSzama { get; set; }
+        public Dictionary<int, int> SikertelenekSzama { get; set; }
+
+
+        public static List<Vizsga> beolvasas()
+        {
+            var output = new List<Vizsga>();
+
+            var sr1 = new StreamReader(@"../../res/sikeres.csv");
+            var sr2 = new StreamReader(@"../../res/sikertelen.csv");
+            sr1.ReadLine();
+            sr2.ReadLine();
+            string[] temp1;
+            string[] temp2;
+            while (!sr1.EndOfStream)
+            {
+
+                temp1 = sr1.ReadLine().Split(';');
+                temp2 = sr2.ReadLine().Split(';');
+                var vizsga = new Vizsga() { Nyelv = temp1[0], SikeresekSzama = new Dictionary<int, int>(), SikertelenekSzama = new Dictionary<int, int>() };
+                for (int i = 2009; i < 2019; i++)
+                {
+                    vizsga.SikeresekSzama.Add(i, Int32.Parse(temp1[i-2008]));
+                    vizsga.SikertelenekSzama.Add(i, Int32.Parse(temp2[i - 2008]));
+                }
+                output.Add(vizsga);
+
+            }
+
+            sr1.Close();
+            sr2.Close();
+
+            return output;
+        }
+    }
+}
